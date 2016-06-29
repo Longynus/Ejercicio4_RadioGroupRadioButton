ea11b06bf498a33fb9bc7f5861dbd53fe8bcdc64
Longynus <hacpin@hotmail.com>
2016-06-29 12:12:44 -0500

    Controles RadioGroup y RadioButton

---
+++ app/src/main/java/happyapps/radiogroupradiobutton/MainActivity.java
@@ -0,0 +1,46 @@
+package happyapps.radiogroupradiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+    private TextView tv3;
+    private RadioButton r1, r2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText) findViewById(R.id.et1);
+        et2=(EditText) findViewById(R.id.et2);
+        tv3=(TextView) findViewById(R.id.tv3);
+        r1=(RadioButton) findViewById(R.id.r1);
+        r2=(RadioButton) findViewById(R.id.r2);
+
+    }
+
+    //este metodo se ejecutara cuando se presione el boton
+    public void operar(View view){
+        int nro1=Integer.parseInt(et1.getText().toString());
+        int nro2=Integer.parseInt(et2.getText().toString());
+
+        if (r1.isChecked()==true){
+            int suma=nro1+nro2;
+            String res= String.valueOf(suma);
+            tv3.setText(res);
+        } else
+            if (r2.isChecked()==true){
+                int resta=nro1-nro2;
+                String res=String.valueOf(resta);
+                tv3.setText(res);
+            }
+    }
+
+}
